fcc4735973e3e2fbdd7c5906947e856db3e22922
khangpham1234 <phamduykhang11a2@gmail.com>
2024-07-03 22:42:00 +0700

    update front-end cart

--- resources/views/frontend/products/partialsDetail/imageGallery.blade.php
+++ resources/views/frontend/products/partialsDetail/imageGallery.blade.php
@@ -1,22 +1,13 @@
-<div class="picture p-3 rounded-1" style="background: #ebebefb3">
-    <img src="{{ asset('uploads/products/' . $product->image) }}" class="w-100 d-block m-0" alt="ảnh iphone">
-</div>
-<div class="picture-slideshow mt-3">
-    <ul class="p-0 d-flex">
-
+<div class="product-image">
+    <div class="big-img">
+        <img src="{{ asset('uploads/products/' . $product->image) }}">
+    </div>
+    <div class="images d-flex">
         @foreach($product_image_galleries as $item)
-            <li class="w-25 list-group-item p-3 border border-dark-subtle rounded-2 me-1">
-                <img src="{{ asset('uploads/gallery/' . $item->image) }}" class="w-100 d-block" alt="thums">
-            </li>
+        <div class="small-img mx-2">
+            <img src="{{ asset('uploads/gallery/' . $item->image) }}" class="img-thumbnail" onclick="showImg(this.src)">
+        </div>
         @endforeach
-        {{-- <li class="w-25 list-group-item p-3 border border-dark-subtle rounded-2 me-1">
-            <img src="public/images/product/iphone_01.png" class="w-100 d-block" alt="thums">
-        </li>
-        <li class="w-25 list-group-item p-3 border border-dark-subtle rounded-2 me-1">
-            <img src="public/images/product/iphone_01.png" class="w-100 d-block" alt="thums">
-        </li>
-        <li class="w-25 list-group-item p-3 border border-dark-subtle rounded-2 me-1">
-            <img src="public/images/product/iphone_01.png" class="w-100 d-block" alt="thums">
-        </li> --}}
-    </ul>
+       
+    </div>
 </div>
--- resources/views/frontend/products/partialsDetail/overView.blade.php
+++ resources/views/frontend/products/partialsDetail/overView.blade.php
@@ -1,101 +1,78 @@
-<div class="wrapper-info py-2 border-bottom">
-    <div class="product-name">
-        <h3 class="fw-medium">{{ $product->name }}</h3>
-    </div>
-    <div class="product-review-box d-flex align-items-center justify-content-between">
-        <div class="d-flex">
-            <div class="rating text-warning pe-2">
-                <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i
-                    class="fa-solid fa-star"></i><i class="fa-regular fa-star"></i>
-            </div>
-        </div>
-        <div class="storelocation-all">
-            <select class="form-select bg-light">
-                <option value="1">Vui lòng chọn</option>
-                <option selected>Khu vực miền Bắc</option>
-                <option value="2">Khu vực miền Nam</option>
-            </select>
-        </div>
-    </div>
-
+<h1 class="title-product fs-4 mt-3">{{ $product->name }}</h1>
+<div class="inventory_quantity">
+    <span class="mb-break">
+        <span class="stock-brand-title">Thương hiệu:</span>
+        <span class="a-vendor">
+            Apple
+        </span>
+    </span>
+    <span class="line">&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+    <span class="mb-break">
+        <span class="stock-brand-title">Tình trạng:</span>
+        <span class="a-stock"><span class="a-stock">Còn hàng</span></span>
+    </span>
 </div>
-<div class="prices d-flex align-items-center my-2">
-    <div class="price me-3 d-flex text-primary">
-        <h4 class="fw-bolder">{{ number_format($product->offer_price, 0, '', ',') }}</h4><i
-            class="fa-solid fa-dong-sign"></i>
-    </div>
-    <div class="lowPrice text-secondary"><del class="fs-5">{{ number_format($product->price, 0, '', ',') }}</del> <i
-            class="fa-light fa-dong-sign fa-xs"></i></div>
+<hr>
+<div class="price-box ">
+    <span class="special-price">
+        <span class="price product-price fs-4 text-danger">19.490.000₫</span>
+    </span>
+    <!-- Giá Khuyến mại -->
+    <span class="old-price">
+        <del class="price product-price-old mx-1">21.990.000₫</del>
+    </span>
+    <!-- Giá gốca -->
 </div>
-<dl class="attributes">
 
-    @if (count($product->variant) > 0)
-        @foreach ($product->variant as $variant)
-            <dt class="text-prompt">
-                <label class="form-label fw-lighter mb-2">{{ $variant->name }}</label>
-            </dt>
-
-            <dd>
-                <ul class="d-flex mb-3">
-
-                    @foreach ($variant->variantItem as $i)
-                        @if ($variant->name == 'Màu sắc')
-                            <li class="list-group-item " ng-show="">
-                                <span class="me-3 rounded-circle d-inline-block"
-                                    style="width: 30px; height: 30px; background: {{ $i->name }};">&nbsp;
-                                </span>
-                            </li>
-                        @else
-                            <li class="list-group-item py-1 px-3 border border-dark-subtle me-2 rounded-2">
-                                <a class="nav-link text-secondary" href="#">{{ $i->name }}</a>
-                            </li>
-                        @endif
-                    @endforeach
-
-                </ul>
-            </dd>
-        @endforeach
-    @endif
-</dl>
-
-<!-- Form addtocart product -->
-<form class="formCart" method="post">
-    <input class="productId" type="hidden" value="{{ $product->id }}">
-    <div class="call-action">
-        <div class="btn-buy my-3">
-            <button type="button" class="btn-addToCart btn text-bg-primary w-100 py-3 fw-bold text-uppercase">Mua
-                ngay</button>
-        </div>
-        <div class="btn-all row">
-            <div class="col-6">
-                <button class="btn btn-outline-primary w-100 py-3 fw-bold text-uppercase">Trả
-                    góp</button>
+<div class="form-product ">
+    <div class="select-swatch mb-3 ">
+        <div class="header">Màu sắc: <span class="value-properties">Xám</span></div>
+        <div class="swatch d-flex">
+            <div data-value="Đỏ" title="Đỏ" class="swatch-element color " onclick="changeColor(this)">
+                <div class="tooltip">Đỏ</div>
+                <input id="swatch-0-den" type="radio" name="option" class="d-none" value="red" checked="">
+                <label for="swatch-0-den" style="background-color: red"></label>
             </div>
-            <div class="col-6">
-                <button class="btn btn-outline-primary w-100 py-3 fw-bold text-uppercase"><i class="fa-solid fa-rotate"></i>
-                    Thu cũ đổi mới</button>
+            <div data-value="Xanh" title="Xanh" class="swatch-element color " onclick="changeColor(this)">
+                <div class="tooltip">Xanh</div>
+                <input id="swatch-0-xanh" type="radio" name="option" class="d-none" value="xanh">
+                <label for="swatch-0-xanh" style="background-color: blue">
+                </label>
+            </div>
+            <div data-value="Vàng" title="Vàng" class="swatch-element color " onclick="changeColor(this)">
+                <div class="tooltip">Vàng</div>
+                <input id="swatch-0-vang" type="radio" name="option" class="d-none" value="vàng">
+                <label for="swatch-0-vang" style="background-color: yellow">
+                </label>
             </div>
         </div>
     </div>
-</form>
-<!-- end form -->
-
-<div class="service py-2 border border-dark-subtle rounded-2 mt-3">
-    <ul>
-        <li class="list-group-item"><i class="fa-solid fa-circle-check" style="color: #0066cc;"></i> Bộ sản phẩm gồm:
-            Hộp, Sách hướng
-            dẫn, Cây lấy sim, Cáp Lightning - Type C</li>
-        <li class="list-group-item"><i class="fa-solid fa-circle-check" style="color: #0066cc;"></i> Bảo hành chính
-            hãng 1 năm <a href="#">(Chi
-                tiết)</a></li>
-        <li class="list-group-item"><i class="fa-solid fa-circle-check" style="color: #0066cc;"></i> Giao hàng nhanh
-            toàn quốc <a href="#">(Chi
-                tiết)</a></li>
-        <li class="list-group-item"><i class="fa-solid fa-circle-check" style="color: #0066cc;"></i> Hoàn thuế cho người
-            nước ngoài <a href="#">(Chi
-                tiết)</a></li>
-        <li class="list-group-item"><i class="fa-solid fa-circle-check" style="color: #0066cc;"></i> Gọi đặt mua <strong
-                class="text-primary">19006626</strong> (7:30 - 22:00)
-        </li>
-    </ul>
+    <div class="product-summary">
+        <div class="rte">
+            <p>✔️Máy mới Fullbox 100% - Chưa Active - Chính Hãng Apple<br> ✔️Được hỗ trợ 1 đổi 1 trong 30 ngày nếu có
+                lỗi từ nhà sản xuất<br> ✔️Bảo hành chính hãng Apple 12 tháng</p>
+        </div>
+    </div>
+    <div class="boz-form mb-3">
+        <div class="flex-quantity">
+            <div class="custom custom-btn-number  ">
+                <span>Số lượng: </span>
+                <div class="input_number_product ">
+                    <button class="btn_num num_1 button button_qty"><span>-</span></button>
+                    <input type="text" id="qtym" name="quantity" value="1" maxlength="3"
+                        class="form-control prd_quantity">
+                    <button class="btn_num num_2 button button_qty"><span>+</span></button>
+                </div>
+            </div>
+            <form class="formCart" method="post">
+                <input class="productId" type="hidden" value="{{ $product->id }}">
+                <div class="btn-mua button_actions col-md-12 col">
+                    <button type="button" class="btn-buyNow btn col-12 btn-addToCart">
+                        <span class="txt-main text-white">Mua ngay</span>
+                        <span class="text-white">Giao tận nơi hoặc nhận tại cửa hàng</span>
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
