1d0f70fc9339320d26797b32b56d3ddead03cad9
sauhv <sau.huynhvan16@gmail.com>
2024-07-05 20:58:52 +0700

    Create table, Orders, order_detail, Product_comment, User_address.

---
+++ database/migrations/2024_07_05_151647_create_orders_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('order_name');
+            $table->string('order_phone',20);
+            $table->string('order_email');
+            $table->string('order_province');
+            $table->string('order_district');
+            $table->string('order_ward');
+            $table->double('total');
+            $table->double('qty_total');
+            $table->string('payment_method');
+            $table->integer('payment_status');
+            $table->text('order_address');
+            $table->text('shipping_method');
+            $table->string('coupon')->nullable();
+            $table->tinyInteger('order_status')->default(0);
+            $table->text('admin_note');
+            $table->text('user_note');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
---
+++ database/migrations/2024_07_05_203459_create_order_details_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_details', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_name');
+            $table->string('variants')->nullable();
+            $table->double('price');
+            $table->integer('qty');
+            $table->unsignedBigInteger('order_id');
+            $table->foreign('order_id')->references('id')->on('orders');
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_details');
+    }
+};
