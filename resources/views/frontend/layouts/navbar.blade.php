93b1f464fc73c067c5765fd5c2f6bd0ffd6e71f8
sauhv <sau.huynhvan16@gmail.com>
2024-07-12 22:50:43 +0700

    edit header

--- resources/views/frontend/layouts/navbar.blade.php
+++ resources/views/frontend/layouts/navbar.blade.php
@@ -1,68 +1,124 @@
-<nav class="navbar navbar-expand-lg menu-mobile">
-    <!-- navbar show web  -->
-    <div class="container-fluid">
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-collapse navbar-nav mb-2 mb-lg-0 justify-content-center">
-                <li class="navbar-toggler nav-item border-0">
-                    <div class="d-flex mt-1 input-search-mobile">
-                        <input class="form-control text-secondary text-opacity-50" type="text"
-                            placeholder="Tìm Sản Phẩm..." aria-label="Search">
-                        <button class="btn"style="margin-left: -40px" type="submit"><i
-                                class="fa-light fa-magnifying-glass fa-lg text-secondary text-opacity-75"></i></button>
-                    </div>
-                </li>
-                <li class="nav-item mxc">
+<nav class="navbar navbar-expand-lg ">
+    <div class="container">
+        <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 mt-1">
+                <li class="nav-item nav_item_cate px-2">
                     <a class="nav-link text-white" aria-current="page" href="/">Trang chủ</a>
-                </li>              
-                <li class="nav-item mxc">
-                    <a class="nav-link text-white" aria-current="page" href="/thong-tin/gioi-thieu/gioi-thieu">Giới thiệu</a>
-                </li> 
-                <li class="navbar-toggler nav-item mxc border-0">
-                    <div class="dropdown">
-                        <span class="dropdown-toggle text-white fs-6" id="dropdownMenuButton1" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            Tài khoản
-                        </span>
-                        <ul class="dropdown-menu dropdown-menu-end rounded-0 p-0" aria-labelledby="dropdownMenuButton1">
-                            <li><a class="dropdown-item border border-top-1 border-bottom-1" href="#">Đăng
-                                    nhập</a></li>
-                            <li><a class="dropdown-item border border-top-1 border-bottom-1" href="#">Đăng ký</a>
+                </li>
+                <li class="nav-item nav_item_cate px-2">
+                    <a class="nav-link text-white" href="#">Giới thiệu</a>
+                </li>
+                <li class="nav-item nav_item_cate px-2 dropdown">
+                    <a class="nav-link dropdown-toggle text-white" href="#" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Sản Phẩm
+                    </a>
+                    <ul class="dropdown-menu mt-1 nav_sub_dropdown p-0">
+                        @foreach ($categories as $category)
+                            <li class="nav_sub_cate">
+                                <a class="dropdown-item item_nav_name py-2" href="#">{{ $category->name }} 
+                                    @if ($category->subCategories->count() > 0)
+                                    <span class="float-end"><i class="fa-solid fa-chevron-right"></i></span>
+                                    @endif
+                                </a>
+                                @if ($category->subCategories->count() > 0)
+                                    <ul class="bg-white dropdown-menu dropdown-submenu p-0">
+                                        @foreach ($category->subCategories as $subCategory)
+                                            <li class="list-group-item">
+                                                <a class="dropdown-item py-2 item_child_nav"
+                                                    href="/product/{{ $category->slug }}/{{ $subCategory->slug }}">{{ $subCategory->name }}</a>
+                                            </li>
+                                        @endforeach
+                                    </ul>
+                                @endif
                             </li>
-                            <li><a class="dropdown-item border border-top-1 border-bottom-1" href="#">Dang sách
-                                    yêu thích (0)</a></li>
-                            <li><a class="dropdown-item border border-top-1 border-bottom-1" href="#">So sánh sản
-                                    phẩm (0)</a></li>
-                        </ul>
-                    </div>
+                        @endforeach
+                    </ul>
                 </li>
-
-                @foreach ($categories as $category)
-                    <li class="nav-item mxc ct-hove">
-                        <div class="dropdown-center rounded-0">
-                            <span class="text-white fs-6 dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-
-                                {{ $category->name }}
-                            </span>
-                            @if ($category->subCategories->count() > 0)
-                                <ul class="dropdown-menu dropdown-menu-end rounded-0 p-0">
-                                    @foreach ($category->subCategories as $subCategory)
-                                        <li class="m-0"><a class="dropdown-item border border-bottom-1"
-                                                href="/product/{{ $category->slug }}/{{ $subCategory->slug }}">{{ $subCategory->name }}</a>
-                                        </li>
-                                    @endforeach
-                                </ul>
-                            @endif
-                        </div>
-                    </li>
-                @endforeach
-                <li class="nav-item mxc">
-                    <a class="nav-link text-white" aria-current="page" href="/tin-tuc">Tin tức</a>
+                <li class="nav-item nav_item_cate px-2">
+                    <a class="nav-link text-white" href="/tin-tuc">Tin tức</a>
                 </li>
-                <li class="nav-item mxc">
-                    <a class="nav-link text-white" aria-current="page" href="#">Liên hệ</a>
+                <li class="nav-item nav_item_cate px-2">
+                    <a class="nav-link text-white" href="#">Liên hệ</a>
+                </li>
+                <li class="nav-item nav_item_cate px-2">
+                    <a class="nav-link text-white" href="#">Chính Sách</a>
+                </li>
+                <li class="nav-item nav_item_cate px-2">
+                    <a class="nav-link text-white" href="#">Đơn Hàng Của Tôi</a>
                 </li>
             </ul>
         </div>
     </div>
-    <!-- navbar show web  -->
 </nav>
+
+<style>
+    .navbar-nav {
+        width: 100%;
+        justify-content: center;
+    }
+
+    .navbar-nav .dropdown-submenu {
+        display: none;
+        position: absolute;
+        top: 0;
+        left: 100%;
+        min-width: 200px;
+        z-index: 1000;
+        margin-top: 20px
+    }
+
+    .navbar-nav .dropdown-menu>li:hover>.dropdown-submenu {
+        display: block;
+    }
+
+    @media (max-width: 991.98px) {
+        .navbar-nav {
+            justify-content: flex-start;
+        }
+
+        .ms-auto {
+            margin-left: auto !important;
+        }
+
+        .dropdown-submenu {
+            display: none;
+        }
+    }
+
+
+
+
+    /* New styles for dropdown hover effect */
+    .dropdown-menu .dropdown-item:hover,
+    .dropdown-menu .dropdown-submenu .dropdown-item:hover {
+        background-color: #000;
+        color: #ffffff;
+    }
+
+    .nav_sub_cate:hover>.item_nav_name {
+        background-color: #000;
+        color: #ffffff;
+    }
+
+    .dropdown-menu {
+        border: none;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+
+    .dropdown-menu .dropdown-submenu {
+        display: none;
+        position: absolute;
+        left: 100%;
+        top: -7px;
+    }
+
+    .dropdown-menu>li:hover>.dropdown-submenu {
+        display: block;
+    }
+
+    /* Ensure submenu is on top of other elements */
+    .dropdown-submenu {
+        z-index: 1000;
+    }
+</style>
