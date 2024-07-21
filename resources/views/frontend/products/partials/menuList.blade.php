c49c5a9ec32a11193f31758ead781de014802952
Trần Thanh Vinh <tranthanhvinh242@email.com>
2024-07-21 11:05:53 +0700

    fix router convert english to vietnamese

--- resources/views/frontend/products/partials/menuList.blade.php
+++ resources/views/frontend/products/partials/menuList.blade.php
@@ -3,7 +3,7 @@
 
         @foreach ($category->subCategories as $item)
             <a class="cate-item text-decoration-none text-center flex-grow-0 flex-shrink-0 flex-basis-auto"
-                href="/product/{{ $category->slug }}/{{ $item->slug }}">
+                href="/san-pham/{{ $category->slug }}/{{ $item->slug }}">
                 <div class="cate-img ">
                     <img width="100" height="100"
                         src="//bizweb.dktcdn.net/thumb/large/100/480/632/collections/230223022516-thiet-ke-chua-co-te.png"
@@ -18,7 +18,7 @@
     @if (isset($subCategory) && $subCategory->childCategory->count() > 0)
         @foreach ($subCategory->childCategory as $item)
             <a class="cate-item text-decoration-none text-center flex-grow-0 flex-shrink-0 flex-basis-auto"
-                href="/product/{{ $subCategory->slug }}/{{ $item->slug }}">
+                href="/san-pham/{{ $subCategory->slug }}/{{ $item->slug }}">
                 <div class="cate-img ">
                     <img width="100" height="100"
                         src="//bizweb.dktcdn.net/thumb/large/100/480/632/collections/230223022516-thiet-ke-chua-co-te.png"
--- resources/views/frontend/products/partialsDetail/breadCrumb.blade.php
+++ resources/views/frontend/products/partialsDetail/breadCrumb.blade.php
@@ -4,19 +4,19 @@
             <li class="home">
                 <a href="/" title="Trang chủ"><span>Trang chủ</span></a>
                 <span class="mr_lr mx-2"><i class="fas fa-angle-right"></i></span>
-                
+
                 @if (isset($category))
                     <span class="mr_lr mx-2"><i class="fas fa-angle-right"></i></span>
-                    <a href="/product/{{ $category->slug }}/"
+                    <a href="/san-pham/{{ $category->slug }}/"
                         title="{{ $category->name }}"><strong><span>{{ $category->name }}</span></strong></a>
                 @endif
                 @if (isset($subCategory))
                     <span class="mr_lr mx-2"><i class="fas fa-angle-right"></i></span>
-                    <a href="/product/{{ $subCategory->category->slug }}/" title="{{ $subCategory->category->name }}">
+                    <a href="/san-pham/{{ $subCategory->category->slug }}/" title="{{ $subCategory->category->name }}">
                         <span>{{ $subCategory->category->name }}</span>
                     </a>
                     <span class="mr_lr mx-2"><i class="fas fa-angle-right"></i></span>
-                    <a href="/product/{{ $subCategory->category->slug }}/{{ $subCategory->slug }}/"
+                    <a href="/san-pham/{{ $subCategory->category->slug }}/{{ $subCategory->slug }}/"
                         title="{{ $subCategory->name }}">
                         <strong>
                             <span>{{ $subCategory->name }}</span>
@@ -26,17 +26,17 @@
 
                 @if (isset($childCategory))
                     <span class="mr_lr mx-2"><i class="fas fa-angle-right"></i></span>
-                    <a href="/product/{{ $childCategory->category->slug }}/"
+                    <a href="/san-pham/{{ $childCategory->category->slug }}/"
                         title="{{ $childCategory->category->name }}">
                         <span>{{ $childCategory->category->name }}</span>
                     </a>
                     <span class="mr_lr mx-2"><i class="fas fa-angle-right"></i></span>
-                    <a href="/product/{{ $childCategory->category->slug }}/{{ $childCategory->subCategory->slug }}/"
+                    <a href="/san-pham/{{ $childCategory->category->slug }}/{{ $childCategory->subCategory->slug }}/"
                         title="{{ $childCategory->subCategory->name }}">
                         <span>{{ $childCategory->subCategory->name }}</span>
                     </a>
                     <span class="mr_lr mx-2"><i class="fas fa-angle-right"></i></span>
-                    <a href="/product/{{ $childCategory->category->slug }}/{{ $childCategory->subCategory->slug }}/{{ $childCategory->slug }}/"
+                    <a href="/san-pham/{{ $childCategory->category->slug }}/{{ $childCategory->subCategory->slug }}/{{ $childCategory->slug }}/"
                         title="{{ $childCategory->name }}">
                         <strong>
                             <span>{{ $childCategory->name }}</span></a>
