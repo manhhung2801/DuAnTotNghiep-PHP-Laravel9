0d1b59f2bf6105a26c2a64d80d8ce10de698b8df
sauhv <sau.huynhvan16@gmail.com>
2024-07-23 21:30:48 +0700

    Update 23/07/2024

--- app/Http/Helper/Helper.php
+++ app/Http/Helper/Helper.php
@@ -11,31 +11,31 @@ class Helper
 
         if ($offer_start_date >= $formattedDate && ($offer_end_date >= $formattedDate || empty($offer_end_date))) {
             echo  '<span class="compare-price">' . number_format($price, 0, '.', '.') . '<i class="fa-regular fa-dong-sign"></i></span>';
-           
-            
+
+
             echo ' <span class="price">' . number_format($offer_price, 0, '.', '.') . "<i class='fa-solid fa-dong-sign'></i> </span>";
         } else {
             echo     ' <span class="price">' . number_format($price, 0, '.', '.') . '<i
         class="fa-solid fa-dong-sign"></i></span>';
-
         }
     }
 
-    
 
-    public static function sale($offer_start_date, $offer_end_date,  $price,  $offer_price) {
+
+    public static function sale($offer_start_date, $offer_end_date,  $price,  $offer_price)
+    {
         $now = Carbon::now();
         $formattedDate = $now->format('Y-m-d');
 
         if ($offer_price !== null && $offer_start_date >= $formattedDate && ($offer_end_date >= $formattedDate || empty($offer_end_date))) {
-          $discountPercentage =  '<span class="flash-sale">Giảm '. number_format((($price - $offer_price) / $price) * 100, 0) . '%'.'</span>';
-          echo $discountPercentage;
+            $discountPercentage =  '<span class="flash-sale">Giảm ' . number_format((($price - $offer_price) / $price) * 100, 0) . '%' . '</span>';
+            echo $discountPercentage;
         } else {
-        //   echo "0%";
+            //   echo "0%";
         }
-      }
+    }
 
-      public static function getProductPrice($product)
+    public static function getProductPrice($product)
     {
         $productPrice = null;
         $currentDate = new DateTime();
@@ -58,7 +58,6 @@ class Helper
 
         return (int) $productPrice;
     }
+    
 }
-
-
 
